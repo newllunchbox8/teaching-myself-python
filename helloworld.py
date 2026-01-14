22d99fcca8a86cc8b86dd9568af9b8b11aa151e0
newllunchbox8 <aschmunk5025@student.pps.net>
2026-01-14 22:33:24 +0000

    made helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+print('hello world')
+print('yeah i guess its not just a hello world script because i added this line\nthats fine tho\n its kinda just me learning how to print in a basic way')
+print('''i mean
+does it reallly matter
+cuz like im learning how to code
+and in the end if im doing that thats all that matters ''')
