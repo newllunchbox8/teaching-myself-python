e4df18b43d65e30c76730678c3640930fb7d6394
lbx <xrmtomatosoup@gmail.com>
2026-01-16 06:52:50 -0800

    fixed the variablesandstuff =_=

--- variablesandstuff.py
+++ variablesandstuff.py
@@ -1,5 +1,5 @@
-value_1 = banana
-value_2 = apple
+value_1 = 'banana'
+value_2 = 'apple'
 print('what is your favorite fruit?')
 print('my favorite fruit is ' + value_1)
 print('what is your second favorite fruit?')
