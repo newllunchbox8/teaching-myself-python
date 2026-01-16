866eb5972713581f57eca0d59f844c14eadfc08a
lbx <xrmtomatosoup@gmail.com>
2026-01-15 20:32:25 -0800

    variables yay :)

---
+++ variables_and_stuff.py
@@ -0,0 +1,6 @@
+value_1 = banana
+value_2 = apple
+print('what is your favorite fruit?')
+print('my favorite fruit is ' + value_1)
+print('what is your second favorite fruit?')
+print('my second favorite fruit is ' + value_2)
